63d6efd4a59dd45aa58bbd03b6de1e0dc892788b
Amasha Das <125284502+amasha-d@users.noreply.github.com>
2026-02-19 14:48:24 -0600

    congfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+const repository = process.env.GITHUB_REPOSITORY ?? "";
+const [owner, repo] = repository.split("/");
+const isUserSite = owner && repo && repo.toLowerCase() === `${owner.toLowerCase()}.github.io`;
+
+export default defineConfig({
+  site: owner ? `https://${owner}.github.io` : "https://example.com",
+  base: process.env.GITHUB_ACTIONS ? (isUserSite ? "/" : `/${repo}/`) : "/",
+});
